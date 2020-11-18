64e77e850c25d64ca0e23b11ef97f42e8d731c27
Lucas <55569386+LucasMonir@users.noreply.github.com>
2020-11-18 17:44:14 -0300

    Update removeRepeated.js

--- Questoes/removeRepeated.js
+++ Questoes/removeRepeated.js
@@ -8,19 +8,13 @@ const removeRepeated = (string, limit) => {
     let chars = string.split('');
 
     for (let i = 0; i < chars.length; i++) {
-        
         for (let j = 0; j < chars.length; j++) {
-            
             if (chars[i] == chars[j]) {
-                
                 if (chars[i + limit] == chars[j]) {
                     chars.splice(i, 1);
                 }
-
             }
-
         }
-
     }
 
     return chars.join('');
